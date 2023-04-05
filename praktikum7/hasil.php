f1dc94ddfe362a9ea5d1746eb9422894b2025a51
amru404 <amru.azzam3@gmail.com>
2023-04-05 15:17:29 +0700

    up tugas

--- praktikum7/hasil.php
+++ praktikum7/hasil.php
@@ -22,21 +22,34 @@
 
         }
 
+        public function BMI()
+        {
+            $pangkat = $_POST['tb'];
+            return $BMI = $BMI = $_POST['bb'] / $pangkat;
+
+        }
+
         public function Hasil()
         {
 
             $pangkat = $_POST['tb'];
             $BMI = $BMI = $_POST['bb'] / $pangkat;
 
-            if ($BMI ) {
-                # code...
+            if ($BMI < 18.5) {
+                return $keterangan = "Kekurangan Berat Badan";
+            }elseif ($BMI >18.5 && $BMI < 24.9) {
+                return $keterangan = "Normal (Ideal)";
+            }elseif ($BMI >25.0 && $BMI < 29.9) {
+                return $keterangan = "Kelebihan Berat Badan";
+            }elseif ($BMI >30.0) {
+                return $keterangan = "Kegemukan (Obesitas)";
             }
         }
         
     }
 
-    $ket1 = new Keterengan($_POST['nama'],$_POST['bb'],$_POST['tb'],$_POST['umur'],$_POST['jk'], $pangkat = $_POST['tb'] * $_POST['tb'],$BMI = $_POST['bb'] / $pangkat);
+    $ket1 = new Keterengan($_POST['nama'],$_POST['bb'],$_POST['tb'],$_POST['umur'],$_POST['jk']);
 
 
-    echo "Nama : $ket1->nama <br> Berat Badan : $ket1->bb <br> Tinggi Badan : $ket1->tb <br> Umur : $ket1->umur <br> Jenis Kelamin : $ket1->jk <br> BMI: $BMI"
+    echo "Nama : $ket1->nama <br> Berat Badan : $ket1->bb <br> Tinggi Badan : $ket1->tb <br> Umur : $ket1->umur <br> Jenis Kelamin : $ket1->jk <br> BMI:" . $ket1->BMI() ."<br> Hasil : " . $ket1->Hasil();
 ?>
